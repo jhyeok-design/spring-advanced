6459473453adf6687feff643408de077ab53ae2c
최정혁 <jeonghyeogc01@gmaile.com>
2025-12-04 19:20:09 +0900

    Lv1 AuthUserArgumentResolver WebMvcConfig 생성 후 적용

---
+++ src/main/java/org/example/expert/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package org.example.expert.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
+    }
+
+
+}
