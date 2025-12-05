7ddb30b8a197be80490f3eec021096a236869fce
최정혁 <jeonghyeogc01@gmaile.com>
2025-12-05 09:20:48 +0900

    Lv5 LoggingCheckAop 요청/응답 json형식 로깅 체크하기

---
+++ src/main/java/org/example/expert/config/LoggingFilter.java
@@ -0,0 +1,29 @@
+package org.example.expert.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        filterChain.doFilter(requestWrapper, responseWrapper);
+
+        responseWrapper.copyBodyToResponse();
+    }
+}
---
+++ src/main/java/org/example/expert/domain/common/aop/LoggingCheckAop.java
@@ -0,0 +1,68 @@
+package org.example.expert.domain.common.aop;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LoggingCheckAop {
+
+    private final ObjectMapper objectMapper;
+
+
+    @Around("execution(* org.example.expert.domain..service.*AdminService.*(..))")
+    public Object executionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            joinPoint.proceed();
+        }
+
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        ContentCachingRequestWrapper requestWrapper = null;
+
+        if (request instanceof ContentCachingRequestWrapper) {
+            requestWrapper = (ContentCachingRequestWrapper) request;
+        }
+
+        String requestBody;
+        if (requestWrapper != null) {
+            byte[] content = requestWrapper.getContentAsByteArray();
+            requestBody = (content.length > 0) ? new String(content, StandardCharsets.UTF_8) : "값이 없습니다.";
+        } else {
+            requestBody = "값이 없습니다.";
+        }
+
+        Object userIdByJwt = request.getAttribute("userId");
+        Long userId = (Long) userIdByJwt;
+
+        LocalDateTime requestTime = LocalDateTime.now();
+
+        String url = request.getRequestURI();
+
+        log.info("user: {} time: {} url: {} requestBody: {}",
+                userId, requestTime, url, requestBody);
+
+        Object result = joinPoint.proceed();
+        String jsonResult = objectMapper.writeValueAsString(result);
+
+        log.info("responseBody: {}", jsonResult);
+
+        return result;
+    }
+}
